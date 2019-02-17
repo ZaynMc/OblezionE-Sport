87d724d6683eb9bec896c34d0a905e75a0253ef0
ZaynMc <theobourrier@outlook.fr>
2019-02-17 12:28:11 +0100

    upd

--- commands/UnMuteCommand.js
+++ commands/UnMuteCommand.js
@@ -31,7 +31,7 @@ module.exports.run = async (bot, message, args, ops) => {
           name: "Oblezion Boss",
           icon_url: "https://i.imgur.com/qQoSPge.jpg"
         },
-        title: `Le joueur <@${tomute.displayName}> à été unmute !`,
+        title: `Le joueur ${tomute.displayName} à été unmute !`,
         timestamp: new Date(),
         footer: {
           icon_url: "https://i.imgur.com/318H4Xw.png",
