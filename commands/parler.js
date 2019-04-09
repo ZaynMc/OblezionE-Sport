deae0ed991bda340e992d933a7e4baf856fa5309
ZaynMc <theobourrier@outlook.fr>
2019-04-09 18:31:10 +0200

    Update parler.js

--- commands/parler.js
+++ commands/parler.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args, ops) => {
 
     message.delete();
 
-    bot.channels.get('506830992494034954').send('A ton avis Kuzan, comment je fais');
+    bot.channels.get('506830992494034954').send('Kuzan, tu aimes le bot ? x)');
 }
 
 module.exports.help = {
