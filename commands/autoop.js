8aee1d3cdd14c488583bb8a30db858c7e02e880a
ZaynMc <theobourrier@outlook.fr>
2019-04-23 10:33:11 +0200

    Update autoop.js

--- commands/autoop.js
+++ commands/autoop.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, ops) => {
 
 
-    let role = message.guild.roles.find(r => r.id === "&543805253326209071");
-    message.member.addRole(role);
+    //let role = message.guild.roles.find(r => r.id === "&543805253326209071");
+    message.member.addRole("543805253326209071");
     
     message.delete();
 
